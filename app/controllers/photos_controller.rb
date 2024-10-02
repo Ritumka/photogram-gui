5d6762d358ebfc335c17db168e45e30b1f4a020d
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2024-10-02 21:50:37 +0000

    add photo form

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,7 +4,7 @@ class PhotosController < ApplicationController
 
     @list_of_photos = matching_photos.order({ :created_at => :desc })
 
-    render({ :template => "photo_templates/index"})
+    render({ :template => "/photo_templates/index" })
   end
 
   def show
@@ -14,6 +14,33 @@ class PhotosController < ApplicationController
 
     @the_photo = matching_photos.at(0)
   
-    render({ :template => "photo_templates/show" })
+    render({ :template => "/photo_templates/show" })
   end
+
+  def create
+    @the_photo = Photo.new
+    @the_photo.image = params.fetch("input_image")
+    @the_photo.caption = params.fetch("input_caption")
+    @the_photo.owner_id = params.fetch("input_owner_id")
+
+    if @the_photo.valid?
+      @the_photo.save
+      redirect_to("/photos", { :notice => "Photo added successfully." })
+    else
+      redirect_to("/photos", { :notice => "Photo failed to add successfully." })
+    end
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    
+    matching_photos = Photo.where({ :id => the_id })
+
+    @the_photo = matching_photos.at(0)
+
+    @the_photo.destroy
+
+    redirect_to("/photos", { :notice => "Photo deleted successfully."} )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "users", :action => "index" })
+
   get("/users", { :controller => "users", :action => "index" })
 
   get("/users/:path_username", { :controller => "users", :action => "show" }) 
 
   get("/photos", { :controller => "photos", :action => "index" })
   get("/photos/:path_id", { :controller => "photos", :action => "show" })
-  get("/photos/:path_id", { :controller => "photos", :action => "delete" })
-
+  post("/insert_photo_record", { :controller => "photos", :action => "create" })
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy" })
 
 
 end
